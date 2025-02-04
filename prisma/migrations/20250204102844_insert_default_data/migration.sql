d6dc90f535446efd54ebf0c8e4684bb5b5486097
Rawaa-Al-Kabbani <rawaa.alkabbani@outlook.com>
2025-02-04 11:31:54 +0100

    Add migration for default data

---
+++ prisma/migrations/20250204102844_insert_default_data/migration.sql
@@ -0,0 +1,11 @@
+-- This is an empty migration.
+
+INSERT INTO synonym
+(id, group_id, word)
+VALUES('655ec0f9-9ba4-41da-beff-ef8e16ecbd9c', 'a0c9562f-179a-49ea-8b1d-378eff882118', 'hello');
+INSERT INTO synonym
+(id, group_id, word)
+VALUES('34d3309c-5453-4957-bbdc-93e5539e5343', 'a0c9562f-179a-49ea-8b1d-378eff882118', 'hi');
+INSERT INTO synonym
+(id, group_id, word)
+VALUES('b6414736-b163-4d12-b208-dab65fc97a1f', 'a0c9562f-179a-49ea-8b1d-378eff882118', 'welcome');
